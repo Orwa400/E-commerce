c10d08582b85f3bfacfa539e90a68a28edf23d49
Orwa400 <matheworwa3@gmail.com>
2024-01-29 11:14:07 +0300

    Add database framework and update routes

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '07574674420722653976'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+login_manager =LoginManager(app)
+login_manager.login_view = 'login'
 
+# Database Models
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+# Forms
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    submit = SubmitFiels('LOgin')
+
+# Routes
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -9,12 +35,40 @@ def home():
 
 @app.route('/products')
 def products():
+    products = Product.query.all()
     return render_template('products.html')
 
 @app.route('/designer')
+@login_required
 def designer():
     return render_template('designer.html')
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            login_user(user)
+            flash('Login successful!', 'succcess')
+            return redirect(url_for("designer"))
+        else:
+            flash('Login unsuccessful. Please check your username.', 'danger')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'info')
+    return redirect(url_for('home'))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
 
